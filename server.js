8d702d65190fe1fe5c5e2164a69604101df45afb
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2016-10-19 12:32:46 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
+// modules =================================================
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/teadb');
+
+// config files
+// var db = require('./config/db');
+
 var app = express();
 
+// set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public')); 
 
+
+// routes ==================================================
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use('/nerd', require('./routes/nerd'));
 
 
-app.listen(8080);
-console.log("App listening on port 8080");
+// start app ===============================================
+var port = process.env.PORT || 8080; 
+app.listen(port);
+
+// expose app           
+exports = module.exports = app;
