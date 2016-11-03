6ae5e7b48dd52ed294ee0a6b9300d78d6e2a0235
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2016-11-03 14:22:49 +0300

    al libs for angular

--- public/js/core.js
+++ public/js/core.js
@@ -1,6 +1,6 @@
 // angular.module('myShop', ['mainController', 'mainService']);
 
-var app = angular.module('myShop', ['ui.router', 'rzModule', 'checklist-model']);
+var app = angular.module('myShop', ['ui.router', 'rzModule', 'btorfs.multiselect']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     
@@ -22,6 +22,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 });
 
 app.controller('teaController', function($scope, $http, TeaService) {
+	$scope.selection = {};
+
 	$scope.slider = {
 	    minValue: 0,
 	    maxValue: 70,
@@ -40,13 +42,137 @@ app.controller('teaController', function($scope, $http, TeaService) {
 	    }
 	};
 
-
 	$scope._data = {};
-	$scope._data.type = ['sinensis', 'assamica'];
-	$scope._data.region = ['Chinese', 'Indian', 'Ceylon', 'Japanese', 'Indochina', 'African', 'Turkish', 'Arabic'];
-	$scope._data.oxidation = ['green', 'black', 'white', 'yellow', 'oolong', 'puer'];
-	$scope._data.leaf = ['big', 'middle', 'small'];
-	$scope._data.label = ['Lipton', 'Dilmah', 'Teabox', 'Greenfield', 'Earl Grey', 'Akbar Tea'];
+	$scope._data.type = [
+		{
+		    "id": "1",
+		    "description": "Китайская разновидность",
+		    "value": "sinensis"
+		},
+		{
+		    "id": "2",
+		    "description": "Ассамская разновидность",
+		    "value": "assamica"
+		}];
+	$scope._data.region = [
+		{
+		    "id": "1",
+		    "description": "Китайский",
+		    "value": "Chinese"
+		},
+		{
+		    "id": "2",
+		    "description": "Индийский",
+		    "value": "Indian"
+		},
+		{
+		    "id": "3",
+		    "description": "Цейлонский",
+		    "value": "Ceylon"
+		},
+		{
+		    "id": "4",
+		    "description": "Японский",
+		    "value": "Japanese"
+		},
+		{
+		    "id": "5",
+		    "description": "Индокитайский",
+		    "value": "Indochina"
+		},
+		{
+		    "id": "6",
+		    "description": "Африканский",
+		    "value": "African"
+		},
+		{
+		    "id": "7",
+		    "description": "Турецкий",
+		    "value": "Turkish"
+		},
+		{
+		    "id": "8",
+		    "description": "Арабский",
+		    "value": "Arabic"
+		}];
+	$scope._data.oxidation = [
+		{
+		    "id": "1",
+		    "description": "Зеленый",
+		    "value": "green"
+		},
+		{
+		    "id": "2",
+		    "description": "Черный",
+		    "value": "black"
+		},
+		{
+		    "id": "3",
+		    "description": "Белый",
+		    "value": "white"
+		},
+		{
+		    "id": "4",
+		    "description": "Жёлтый",
+		    "value": "yellow"
+		},
+		{
+		    "id": "5",
+		    "description": "Улун",
+		    "value": "oolong"
+		},
+		{
+		    "id": "6",
+		    "description": "Пуэр",
+		    "value": "puer"
+		}];
+	$scope._data.leaf = [
+		{
+		    "id": "1",
+		    "description": "Крупный",
+		    "value": "big"
+		},
+		{
+		    "id": "2",
+		    "description": "Средний",
+		    "value": "middle"
+		},
+		{
+		    "id": "3",
+		    "description": "Мелкий",
+		    "value": "small"
+		}];
+	$scope._data.label = [
+		{
+		    "id": "1",
+		    "description": "Lipton",
+		    "value": "Lipton"
+		},
+		{
+		    "id": "2",
+		    "description": "Dilmah",
+		    "value": "Dilmah"
+		},
+		{
+		    "id": "3",
+		    "description": "Teabox",
+		    "value": "Teabox"
+		},
+		{
+		    "id": "4",
+		    "description": "Greenfield",
+		    "value": "Greenfield"
+		},
+		{
+		    "id": "5",
+		    "description": "Earl Grey",
+		    "value": "Earl Grey"
+		},
+		{
+		    "id": "6",
+		    "description": "Akbar Tea",
+		    "value": "Akbar Tea"
+		}];
 
 	$scope.formData = { 
 		type : angular.copy($scope._data.type),
@@ -67,12 +193,23 @@ app.controller('teaController', function($scope, $http, TeaService) {
 		$scope.formData.price.to = v;
 	});
 
-	$scope.checkAll = function(prop) {
-		$scope.formData[prop] = angular.copy($scope._data[prop]);
-	}
+	// $scope.checkAll = function(prop) {
+	// 	$scope.formData[prop] = angular.copy($scope._data[prop]);
+	// }
 
-	$scope.uncheckAll = function(prop) {
-		$scope.formData[prop] = []; 
+	// $scope.uncheckAll = function(prop) {
+	// 	$scope.formData[prop] = []; 
+	// }
+
+	$scope.setData = function() {
+		return { 
+			type : $scope.formData.type.map(function(a) {return a.value;}),
+			region : $scope.formData.region.map(function(a) {return a.value;}),
+			oxidation : $scope.formData.oxidation.map(function(a) {return a.value;}),
+			leaf : $scope.formData.leaf.map(function(a) {return a.value;}),
+			label : $scope.formData.label.map(function(a) {return a.value;}),
+			price: $scope.formData.price
+		};
 	}
 
 	$scope.setIndex = function(direction) {
@@ -93,8 +230,9 @@ app.controller('teaController', function($scope, $http, TeaService) {
 
 	$scope.setParams = function(direction) {
 		var params = {};
-		params.filter = $scope.formData;
+		params.filter = $scope.setData();
 		params.index = $scope.setIndex(direction);
+		console.log(params);
 		return params;
 	}
 
